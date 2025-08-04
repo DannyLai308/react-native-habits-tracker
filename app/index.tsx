4efbca487da7b9c7e29264a35b0f16a3470c8343
DannyLai308 <56664017+DannyLai308@users.noreply.github.com>
2025-08-03 22:23:17 -0400

    initial project setup

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Here is link to Login Page</Text>
+      <Link href="/login"> Login Page </Link>
+    </View>
+  );
+}
